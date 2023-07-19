b10092bad176566f8aff4fcf5a6e0515067e091c
PekruPienyu <74540743+PekruPienyu@users.noreply.github.com>
2023-07-19 15:17:51 +0530

    Code updates

--- 2D Platformer/Assets/Mario1 1/Characters/Player/Player_Controller.cs
+++ 2D Platformer/Assets/Mario1 1/Characters/Player/Player_Controller.cs
@@ -12,9 +12,7 @@ public class Player_Controller : MonoBehaviour
 
     public float moveSpeed;
     private Vector3 moveDir;
-    private bool isJumping;
-    private bool onAir;
-    public float jumpForce;
+    private bool isGrounded;
 
     private void Awake()
     {
@@ -36,10 +34,11 @@ public class Player_Controller : MonoBehaviour
             moveDir.x = Input.GetAxis("Horizontal");
         }
         GroundCheck();
-        if (Input.GetKeyDown(KeyCode.Space) && !onAir)
+        if (Input.GetKeyDown(KeyCode.Space) && currentJumpState==PlayerState.Grounded)
         {
             anim.Play("Jump");
-            isJumping = true;
+            timer = 0;
+            currentJumpState = PlayerState.Jump;
         }
         else
         {
@@ -54,11 +53,11 @@ public class Player_Controller : MonoBehaviour
                     transform.localScale = new Vector2(-1, 1);
 
                 }
-                if (!onAir) anim.Play("Run");
+               // if (!onAir) anim.Play("Run");
             }
             if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
             {
-                if (!onAir) anim.Play("Idle");
+                //if (!onAir) anim.Play("Idle");
             }
         }
         
@@ -67,38 +66,71 @@ public class Player_Controller : MonoBehaviour
     float timer = 0f;
     float duration = 1f;
 
+    public float jumpSpeed = 5;
+    public float fallAcceleration =1;
+    public float maxFallSpeed = 5;
+    public float jumpDuration = 0.5f;
+
+    private enum PlayerState
+    {
+        Jump,
+        Fall,
+        Grounded
+    }
+    private PlayerState currentJumpState;
+
     private void FixedUpdate()
     {
-        if (isJumping)
+        switch (currentJumpState)
         {
-            timer += Time.deltaTime;
-            float normalizedTime = timer / duration;
-            float strength = jumpForceCurve.Evaluate(normalizedTime);
-            moveDir.y = jumpForce * strength;
-            rb.velocity  = moveDir * Time.deltaTime;
-            if (moveDir.y == 0)
-            {
-                timer = 0;
-                isJumping = false;
-            }
-            onAir = true;
+            case PlayerState.Jump:
+                timer += Time.deltaTime;
+                float normalizedTime = timer / jumpDuration;
+                float strength = jumpForceCurve.Evaluate(normalizedTime);
+                moveDir.y = jumpSpeed * strength;
+                if (timer > jumpDuration)
+                {
+                    timer = 0;
+                    moveDir.y = 0;
+                    currentJumpState = PlayerState.Fall;
+                }
+                break;
+            case PlayerState.Fall:
+                moveDir.y -= fallAcceleration;
+                if(moveDir.y < maxFallSpeed)
+                {
+                    moveDir.y = -maxFallSpeed;
+                }
+                if(isGrounded)
+                {
+                    currentJumpState = PlayerState.Grounded;
+                }
+                break;
+            case PlayerState.Grounded:      
+                moveDir.y = 0;
+                if (!isGrounded)
+                {
+                    currentJumpState = PlayerState.Fall;
+                }
+                break;
+            default:
+                break;
         }
 
-        rb.velocity = moveSpeed * Time.deltaTime * moveDir;
+        //moveDir.y = rb.velocity.y;
+        rb.velocity = moveSpeed * moveDir;
     }
 
     private void GroundCheck()
     {
         RaycastHit2D ray = Physics2D.Raycast(boxCol.bounds.center, Vector2.down, boxCol.bounds.extents.y + 0.1f, layerMask);
-
         if (ray.collider == null || !ray.collider.CompareTag("Ground"))
         {
-            onAir = true;
+            isGrounded = false;
         }
         else
         {
-            if(onAir)anim.Play("Idle");
-            onAir = false;
+            isGrounded = true;
         }  
     }
 }
